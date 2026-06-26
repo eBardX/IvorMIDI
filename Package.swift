ea792f5ef19aae7dbdea1e55fc6dc5d463cbdbe8
John Pusey <ebardx@gmail.com>
2026-06-26 16:33:17 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 
 // © 2025–2026 John Gary Pusey (see LICENSE.md)
 
@@ -18,7 +18,7 @@ let package = Package(name: "IvorMIDI",
                       products: [.library(name: "IvorMIDI",
                                           targets: ["IvorMIDI"])],
                       dependencies: [.package(url: "https://github.com/eBardX/XestiTools.git",
-                                              .upToNextMajor(from: "7.5.0"))],
+                                              .upToNextMajor(from: "9.0.0"))],
                       targets: [.target(name: "IvorMIDI",
                                         dependencies: [.product(name: "XestiTools",
                                                                 package: "XestiTools")],
